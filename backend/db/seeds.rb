1d9e2a35025dd9ccda4f4af34a4f2bcc48a93893
EthanJF <ethanjosephfournier@gmail.com>
2019-11-08 12:31:18 -0500

    set up basic fetch and grid

--- backend/db/seeds.rb
+++ backend/db/seeds.rb
@@ -1,9 +1,9 @@
-records = RestClient.get 'https://random.dog/doggos'
+records = RestClient.get 'https://random.dog/doggos?filter=mp4,webm'
 
 parsedRecords = JSON.parse(records)
 
 500.times do
-    Dog.create(image_url: parsedRecords.sample)
+    Dog.create(image_url: "https://random.dog/#{parsedRecords.sample}")
 end
 
 puts "Seeded!"
